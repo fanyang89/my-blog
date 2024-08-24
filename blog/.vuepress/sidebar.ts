8edb3b67a5b57f7f35000e1f63538001ff31e270
fanyang <fanyang89@outlook.com>
2024-08-24 16:47:45 +0800

    Update sidebar

--- blog/.vuepress/sidebar.ts
+++ blog/.vuepress/sidebar.ts
@@ -3,6 +3,11 @@ import { sidebar } from "vuepress-theme-hope";
 export default sidebar({
   "/": [
     {
+      text: "收藏夹",
+      icon: "bookmark",
+      link: "/fav.html",
+    },
+    {
       text: "文章",
       icon: "book",
       prefix: "posts/",
