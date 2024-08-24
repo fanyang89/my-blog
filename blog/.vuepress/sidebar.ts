6376f3aefbeb0909a89edce40b9176baea58ea4f
fanyang <fanyang89@outlook.com>
2024-08-25 03:50:12 +0800

    Update sidebar

--- blog/.vuepress/sidebar.ts
+++ blog/.vuepress/sidebar.ts
@@ -1,23 +1,37 @@
 import { sidebar } from "vuepress-theme-hope";
 
+const common = [
+  {
+    text: "碎碎念",
+    icon: "memory",
+    link: "/memos.html",
+  },
+  {
+    text: "收藏夹",
+    icon: "bookmark",
+    link: "/fav.html",
+  },
+];
+
 export default sidebar({
-  "/": [
+  "/posts/": [
+    ...common,
     {
-      text: "碎碎念",
-      icon: "memory",
-      link: "/memos.html",
+      text: "文章",
+      icon: "book",
+      children: "structure",
     },
-    {
-      text: "收藏夹",
-      icon: "bookmark",
-      link: "/fav.html",
-    },
+  ],
+  "/devops/": [
+    ...common,
     {
       text: "运维",
       icon: "network-wired",
-      prefix: "devops/",
       children: "structure",
     },
+  ],
+  "/": [
+    ...common,
     {
       text: "文章",
       icon: "book",
